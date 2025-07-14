647ffd2091be76e152dfc539d9eb0cbe277f9a6d
Lawijayaaa <166088083+Lawijayaaa@users.noreply.github.com>
2025-07-14 11:51:50 +0700

    Update ADCTest.py
    Adjust ADC Test

--- test/ADCTest.py
+++ test/ADCTest.py
@@ -6,15 +6,19 @@ adc = Adafruit_ADS1x15.ADS1115(address = 0x48, busnum = 1)
 while True:
     #ANcurrent3 = adc.read_adc(3, gain = 2)
     #ANcurrent3 = ((adc.read_adc(3, gain = 2)) * 2.048 * 2.5) / 32767
-    ANcurrent3 = adc.read_adc(3, gain = 2) * 0.06393945
-    ANcurrent2 = ((adc.read_adc(2, gain = 2)) * 2.048 * 2.5) / 32767
-    ANvoltage1 = adc.read_adc(1, gain = 2)
+    #ANcurrent3 = adc.read_adc(3, gain = 2) * 0.06393945
+    #ANcurrent2 = ((adc.read_adc(2, gain = 2)) * 2.048 * 2.5) / 32767
+    #ANvoltage1 = adc.read_adc(1, gain = 2)
     #ANvoltage1 = ((adc.read_adc(1, gain = 2)) * 5.12) / 32767
     #ANvoltage0 = ((adc.read_adc(0, gain = 2)) * 5.12) / 32767
+    ANcurrent3 = adc.read_adc(3, gain = 2)    #Temperature
+    ANcurrent2 = adc.read_adc(2, gain = 2)    #Pressure
+    ANvoltage1 = adc.read_adc(1, gain = 2)    #OIL Level Alarm
+    ANvoltage1 = adc.read_adc(0, gain = 2)    #OIL Level Trip
     print(ANcurrent3)
     print(ANcurrent2)
-    #print(ANvoltage1)
-    #print(ANvoltage0)
+    print(ANvoltage1)
+    print(ANvoltage0)
     print("~~")
     sleep(1)
 
